63c9863a503c5fac4d09c99f529e5c20f0b00194
Asher Etherington <asherce21@gmail.com>
2023-08-16 18:55:54 -0700

    Update tree_traversal.py

---
+++ tree_traversal.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while (temp):
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+    #start at top and go down layer by layer
+    def breadth_first_search(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+        
+
+
+
+    #depth_first_search
+
+
+bst = BinarySearchTree()
+bst.insert(5)
+bst.insert(7)
+bst.insert(4)
+bst.insert(9)
+print('\n',bst.breadth_first_search(),'\n')
